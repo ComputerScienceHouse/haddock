004f4dfc86c52da422e42f9d14889e70b0ec7659
Galen Guyer <galen@galenguyer.com>
2020-10-28 16:28:29 -0400

    add some functions to simplify stuff and make it safer

--- main.go
+++ main.go
@@ -29,35 +29,57 @@ func main() {
 	log.Println("words.txt read")
 
 	words := make(map[int][]string)
+	longestWord := 0
 
 	for _, word := range lines {
 		words[len(word)] = append(words[len(word)], word)
+		if len(word) > longestWord {
+			longestWord = len(word)
+		}
 	}
 
-	log.Println(GeneratePassword(words, 32))
+	log.Println(GeneratePassword(words, longestWord, 16))
+	log.Println(GeneratePassword(words, longestWord, 16))
+	log.Println(GeneratePassword(words, longestWord, 16))
+	log.Println(GeneratePassword(words, longestWord, 24))
+	log.Println(GeneratePassword(words, longestWord, 24))
+	log.Println(GeneratePassword(words, longestWord, 48))
+	log.Println(GeneratePassword(words, longestWord, 48))
+	log.Println(GeneratePassword(words, longestWord, 48))
 }
 
-func GeneratePassword(words map[int][]string, length int) string {
-	wordone := GetRandomWord(words)
-	wordtwo := GetRandomWordWithLength(words, length-len(wordone)-2)
-	finalpassword := wordone + GetRandomDigit() + GetRandomSymbol() + wordtwo
+// length MUST be above 16
+func GeneratePassword(words map[int][]string, longestWord int, length int) string {
+	var min int
+	if length <= 16 {
+		min = 6
+	} else if length <= 24 {
+		min = 8
+	} else if length <= 32 {
+		min = 12
+	} else {
+		min = 14
+	}
+	lenone := GetRandomNumberBetween(min, ((length) / 2))
+	lentwo := GetRandomNumberBetween(min, ((length) / 2))
+	wordone := GetRandomWordWithLength(words, lenone)
+	wordtwo := GetRandomWordWithLength(words, lentwo)
+	finalpassword := wordone + GetRandomDigit()
+	for len(finalpassword) < (length - lentwo - 1) {
+		finalpassword = finalpassword + GetRandomDigit()
+	}
+	finalpassword = finalpassword + GetRandomSymbol() + wordtwo
 	return finalpassword
 }
 
-func GetRandomWord(words map[int][]string) string {
-	i, err := rand.Int(rand.Reader, big.NewInt(int64(len(words))))
-	if err != nil {
-		log.Fatal(err)
-	}
-	wordArray := words[int(i.Int64())]
-	i, err = rand.Int(rand.Reader, big.NewInt(int64(len(wordArray))))
-	if err != nil {
-		log.Fatal(err)
-	}
-	return wordArray[int(i.Int64())]
-}
 func GetRandomWordWithLength(words map[int][]string, length int) string {
-	wordArray := words[length]
+	var safelen int
+	if length > 23 {
+		safelen = 22
+	} else {
+		safelen = length
+	}
+	wordArray := words[safelen]
 	i, err := rand.Int(rand.Reader, big.NewInt(int64(len(wordArray))))
 	if err != nil {
 		log.Fatal(err)
@@ -83,4 +105,20 @@ func GetRandomSymbol() string {
 		log.Fatal(err)
 	}
 	return string(symbols[int(i.Int64())])
+}
+
+func GetRandomNumberBetween(min int, max int) int {
+	num := (GetRandomNumber() % max) + 1
+	for num < min || max < num {
+		num = GetRandomNumber()
+	}
+	return num
+}
+
+func GetRandomNumber() int {
+	i, err := rand.Int(rand.Reader, big.NewInt(63))
+	if err != nil {
+		log.Fatal(err)
+	}
+	return int(i.Int64()) + 1
 }
