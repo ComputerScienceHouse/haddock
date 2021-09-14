665c31561e5b2ebd3d664aeb996637f779978946
Will Nilges <wdn5796@rit.edu>
2021-09-13 23:30:53 -0400

    Change how passwords are generated
    Bro use a for loop lol.

--- main.go
+++ main.go
@@ -77,19 +77,14 @@ func handleGeneratePassword(w http.ResponseWriter, r *http.Request) {
 		length = 48
 	}
 
-	var data []string
-	data = []string{
-		GeneratePassword(length),
-		GeneratePassword(length),
-		GeneratePassword(length),
-		GeneratePassword(length),
-		GeneratePassword(length),
-		GeneratePassword(length),
-		GeneratePassword(length),
-		GeneratePassword(length),
-		GeneratePassword(length),
-		GeneratePassword(length),
+	passwordCount := 10
+
+	data := make([]string, passwordCount)
+
+	for i := range data {
+		data[i] = GeneratePassword(length)
 	}
+
 	json.NewEncoder(w).Encode(data)
 }
 
@@ -113,19 +108,14 @@ func handleGenerateXKCDPassword(w http.ResponseWriter, r *http.Request) {
 		length = 64
 	}
 
-	var data []string
-	data = []string{
-		GenerateXKCDPassword(length),
-		GenerateXKCDPassword(length),
-		GenerateXKCDPassword(length),
-		GenerateXKCDPassword(length),
-		GenerateXKCDPassword(length),
-		GenerateXKCDPassword(length),
-		GenerateXKCDPassword(length),
-		GenerateXKCDPassword(length),
-		GenerateXKCDPassword(length),
-		GenerateXKCDPassword(length),
+	passwordCount := 10
+
+	data := make([]string, passwordCount)
+
+	for i := range data {
+		data[i] = GenerateXKCDPassword(length)
 	}
+
 	json.NewEncoder(w).Encode(data)
 }
 
